729a5b5737a6d89b33029dd9203216b866a6a97e
Visal Dinuwara <visaldinuwara@gmail.com>
2026-04-27 21:27:46 +0530

    Use axios to fetch the data

--- ecommerce-frontend/src/pages/HomePage.jsx
+++ ecommerce-frontend/src/pages/HomePage.jsx
@@ -1,8 +1,13 @@
+import axios from 'axios';
 import { Header } from '../components/Header';
-import { products } from '../../starting-code/data/products';
+import { products } from '../../../starting-code/data/products';
 import './HomePage.css';
 
 export function HomePage() {
+  axios.get('http://localhost:3000/api/products')
+    .then((response)=>{
+      console.log(response.data);
+      })
   return (
     <>
       <title>Ecommerce Project</title>
