090c747d1d674da0ebd7ea31e8454f225bab0455
Visal Dinuwara <visaldinuwara@gmail.com>
2026-04-28 02:36:01 +0530

    Generate HTMl for cart items

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -1,25 +1,24 @@
 import { Routes, Route } from 'react-router';
 import axios from 'axios';
-import { useState,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPage';
 import './App.css'
-import { useEffect } from 'react';
 
 function App() {
   const [cart, setCart] = useState([]);
 
-  useEffect(()=>{
-  axios.get('/api/cart-items')
-    .then((response) => {
-      setCart(response.data);
-    })
-    },[]);
+  useEffect(() => {
+    axios.get('/api/cart-items?expand=product')
+      .then((response) => {
+        setCart(response.data);
+      })
+  }, []);
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart}/>} />
-      <Route path="checkout" element={<CheckoutPage cart={cart}/>} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage />} />
     </Routes>
   )
--- ecommerce-frontend/src/pages/CheckoutPage.jsx
+++ ecommerce-frontend/src/pages/CheckoutPage.jsx
@@ -1,7 +1,8 @@
 import './checkout-header.css';
 import './CheckoutPage.css';
+import { formatMoney } from '../utils/money';
 
-export function CheckoutPage() {
+export function CheckoutPage({cart}) {
   return (
     <>
       <title>Checkout</title>
@@ -31,6 +32,85 @@ export function CheckoutPage() {
 
         <div className="checkout-grid">
           <div className="order-summary">
+            {cart.map((cartItem) => {
+              return (
+                <div key={cartItem.productId} className="cart-item-container">
+                  <div className="delivery-date">
+                    Delivery date: Tuesday, June 21
+                  </div>
+
+                  <div className="cart-item-details-grid">
+                    <img className="product-image"
+                      src={cartItem.product.image} />
+
+                    <div className="cart-item-details">
+                      <div className="product-name">
+                        {cartItem.product.name}
+                      </div>
+                      <div className="product-price">
+                        {formatMoney(cartItem.product.priceCents)}
+                      </div>
+                      <div className="product-quantity">
+                        <span>
+                          Quantity: <span className="quantity-label">{cartItem.quantity}</span>
+                        </span>
+                        <span className="update-quantity-link link-primary">
+                          Update
+                        </span>
+                        <span className="delete-quantity-link link-primary">
+                          Delete
+                        </span>
+                      </div>
+                    </div>
+
+                    <div className="delivery-options">
+                      <div className="delivery-options-title">
+                        Choose a delivery option:
+                      </div>
+                      <div className="delivery-option">
+                        <input type="radio" checked
+                          className="delivery-option-input"
+                          name="delivery-option-1" />
+                        <div>
+                          <div className="delivery-option-date">
+                            Tuesday, June 21
+                          </div>
+                          <div className="delivery-option-price">
+                            FREE Shipping
+                          </div>
+                        </div>
+                      </div>
+                      <div className="delivery-option">
+                        <input type="radio"
+                          className="delivery-option-input"
+                          name="delivery-option-1" />
+                        <div>
+                          <div className="delivery-option-date">
+                            Wednesday, June 15
+                          </div>
+                          <div className="delivery-option-price">
+                            $4.99 - Shipping
+                          </div>
+                        </div>
+                      </div>
+                      <div className="delivery-option">
+                        <input type="radio"
+                          className="delivery-option-input"
+                          name="delivery-option-1" />
+                        <div>
+                          <div className="delivery-option-date">
+                            Monday, June 13
+                          </div>
+                          <div className="delivery-option-price">
+                            $9.99 - Shipping
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
             <div className="cart-item-container">
               <div className="delivery-date">
                 Delivery date: Tuesday, June 21
